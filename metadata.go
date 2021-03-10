344b7fd7d53661f1291225843efcd2c00c432a06
tint <46518383+tint@users.noreply.github.com>
2021-03-10 14:42:18 +0800

    fix(*): 初始化和设置值时使用原始键名

--- metadata.go
+++ metadata.go
@@ -1,18 +1,3 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// Original source: github.com/micro/go-micro/v3/metadata/metadata.go
-
-// Package metadata is a way of defining message headers
 package metadata
 
 import (
@@ -27,8 +12,7 @@ type Metadata map[string]string
 // New creates an Metadata from a given key-value map.
 func New(m map[string]string) Metadata {
 	md := Metadata{}
-	for k, val := range m {
-		key := strings.Title(k)
+	for key, val := range m {
 		md[key] = val
 	}
 	return md
@@ -52,7 +36,7 @@ func (md Metadata) Get(key string) (string, bool) {
 }
 
 func (md Metadata) Set(key, val string) {
-	md[strings.Title(key)] = val
+	md[key] = val
 }
 
 func (md Metadata) Delete(keys ...string) {
